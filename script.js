ee831dbeae03ef64106bb2d58f0a66e1303f262b
Liufat <dd5672003@gmail.com>
2023-02-03 08:23:34 +0800

    [新增]太高、太低、猜中

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 'use strict';
+
+const answer = Math.trunc(Math.random() * 100) + 1;
+// console.log(number);
+document.querySelector('.number').textContent = answer;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number !';
+  } else if (guess === answer) {
+    document.querySelector('.message').textContent = '👍 Correct Number !';
+  } else if (guess > answer) {
+    document.querySelector('.message').textContent = '⬆️ Too High !';
+  } else if (guess < answer) {
+    document.querySelector('.message').textContent = '⬇️ Too Low !';
   }
 });
