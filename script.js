fce23cc342d17ec975049d33a1620eafef232a46
Liufat <dd5672003@gmail.com>
2023-02-03 09:01:16 +0800

    [新增]分數、猜中時背景變色、數字框變長 [修改]猜中時才會顯示答案數字

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 'use strict';
 
-const answer = Math.trunc(Math.random() * 100) + 1;
+const answer = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
 // console.log(number);
-document.querySelector('.number').textContent = answer;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
+  if (score > 1) {
+    if (!guess) {
+      document.querySelector('.message').textContent = '⛔ No Number !';
+    } else if (guess === answer) {
+      document.querySelector('body').style.backgroundColor = '#60b347';
 
-  if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No Number !';
-  } else if (guess === answer) {
-    document.querySelector('.message').textContent = '👍 Correct Number !';
-  } else if (guess > answer) {
-    document.querySelector('.message').textContent = '⬆️ Too High !';
-  } else if (guess < answer) {
-    document.querySelector('.message').textContent = '⬇️ Too Low !';
+      document.querySelector('.number').style.width = '30rem';
+
+      document.querySelector('.number').textContent = answer;
+
+      document.querySelector('.message').textContent = '👍 Correct Number !';
+    } else if (guess > answer) {
+      document.querySelector('.message').textContent = '⬆️ Too High !';
+      score--;
+
+      document.querySelector('.score').textContent = score;
+    } else if (guess < answer) {
+      document.querySelector('.message').textContent = '⬇️ Too Low !';
+      score--;
+
+      document.querySelector('.score').textContent = score;
+    }
+  } else {
+    document.querySelector('.message').textContent = 'You Lost The Game';
+
+    document.querySelector('.score').textContent = 0;
   }
 });
